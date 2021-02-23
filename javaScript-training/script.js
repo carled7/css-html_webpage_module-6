5c77a96494570bd4a028210e861c678dda19ab7d
carled7 <carlos_ed03@outlook.com>
2021-02-23 11:56:23 -0300

    JS test page

---
+++ javaScript-training/script.js
@@ -0,0 +1,14 @@
+
+
+function overButton(x){
+    x.style.padding = "10px"
+    x.style.borderRadius = "7px"
+    x.style.color = "white"
+    x.style.backgroundColor = "red"
+}
+function outButton(y){
+    y.style.padding = "8px"
+    y.style.borderRadius = "3px"
+    y.style.color = "#262626"
+    y.style.backgroundColor = "lightblue"
+}
