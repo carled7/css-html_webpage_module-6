f972625424c19a79587003d3c7ef2653ebfb862f
carled7 <carlos_ed03@outlook.com>
2021-02-26 08:31:02 -0300

    Update barbecue calculator

--- Barbeque-calculator/script.js
+++ Barbeque-calculator/script.js
@@ -5,19 +5,18 @@ function calculator() {
     let hours = document.getElementById("duration");
 
     //Meat Calculator
-    console.log(totalMeat = ((a.value * 400 + (c.value * 200)) * (hours.value / 2)) / 1000);
-    let meat = document.getElementsByTagName("p")[0];
+    let totalMeat = ((a.value * 400 + (c.value * 200)) * (hours.value / 2)) / 1000;
+    let meat = document.getElementsByTagName("li")[0];
     meat.innerText = "Amount of meat: " + totalMeat + "Kg";
 
     //Drink Calculator
-    console.log(totalDrink = ((a.value * 1500 + c.value * 750) * (hours.value / 2)) / 1000);
-    let drink = document.getElementsByTagName("p")[1];
+    let totalDrink = Math.ceil(((a.value * 1500 + c.value * 750) * (hours.value / 2)) / 1000);
+    let drink = document.getElementsByTagName("li")[1];
     drink.innerText = "Amount of drink: " + totalDrink + "l";
 
     //Garlic Bread Calculator
     var totalGarlicBread = (parseInt(a.value) + parseInt(c.value)) * hours.value;
-    console.log(totalGarlicBread)
-    let garlicBread = document.getElementById("p3")
+    let garlicBread = document.getElementsByTagName("li")[2];
     garlicBread.innerText = "Amount of garlic bread: " + totalGarlicBread;
 }
 function efect(x){
