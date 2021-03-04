5893c619fa4fc9c950080b5ab96db15631f759e0
carled7 <carlos_ed03@outlook.com>
2021-03-04 11:58:43 -0300

    Add to canvas test

--- draft-files/module-11/test 03/script.js
+++ draft-files/module-11/test 03/script.js
@@ -1,26 +1,69 @@
- let display = document.getElementById("display");
- let ctx = display.getContext("2d");
+let display = document.getElementById("display");
+let ctx = display.getContext("2d");
 
- display.style.backgroundColor = "#F2F2F2";
+display.style.backgroundColor = "#F2F2F2";
 
- ctx.beginPath();
-    ctx.moveTo(10, 30);
-    ctx.lineTo(1260, 30);
-    ctx.lineWidth = "2";
-    ctx.strokeStyle = "blue";
+ctx.beginPath();
+ctx.moveTo(10, 30);
+ctx.lineTo(1260, 30);
+ctx.lineWidth = "2";
+ctx.strokeStyle = "blue";
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(10, 50);
+ctx.lineTo(1260, 50);
+ctx.lineWidth = "5";
+ctx.strokeStyle = "red";
+ctx.stroke();
+
+ctx.beginPath();
+ctx.fillStyle = "gray";
+ctx.fillRect(10, 70, 1250, 100);
+
+ctx.beginPath();
+ctx.fillStyle = "black";
+ctx.fillRect(10, 190, 1250, 100);
+
+let circle = {
+    x: 630,
+    y: 500,
+    radius: 100,
+    startPoint: 0,
+    endPoint: 0,
+    color: "rgba(235, 0, 250, 0.8)"
+}
+
+function drawCircle(c) {
+
+    ctx.beginPath();
+    ctx.fillStyle = "#F2F2F2";
+    ctx.rect((c.x - c.radius) - (c.radius * 0.2), (c.y - c.radius) - (c.radius * 0.2), (c.radius * 2) + (c.radius * 0.3), (c.radius * 2) + (c.radius * 0.3));
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.strokeStyle = "purple";
+    ctx.lineWidth = "1";
+    ctx.fillStyle = c.color;
+    ctx.arc(c.x, c.y, c.radius, c.startPoint, c.endPoint);
+    ctx.fill();
     ctx.stroke();
+}
+setInterval(function () {
 
- ctx.beginPath();       
-    ctx.moveTo(10, 50);
-    ctx.lineTo(1260, 50);
-    ctx.lineWidth = "5";
-    ctx.strokeStyle = "red";
-    ctx.stroke();
+    if (circle.endPoint < 2 * Math.PI) {
+        circle.endPoint += 0.2;
+        circle.x += 10;
+    }
+    drawCircle(circle);
+}, 16.67);
 
- ctx.beginPath();
-    ctx.fillStyle = "gray";
-    ctx.fillRect(10, 70, 1250, 100);
+let img = new Image();
+img.src = "image.webp";
 
- ctx.beginPath();
-    ctx.fillStyle = "black";
-    ctx.fillRect(10, 190, 1250, 100);
+img.onload = draw;
+
+function draw() {
+
+    ctx.drawImage(this, 10, 300, this.naturalWidth / 3, this.naturalHeight / 3);
+}
