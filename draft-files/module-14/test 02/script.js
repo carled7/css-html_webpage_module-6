fd25c10c9c1e1ca79011dde28f33416048985f68
carled7 <carlos_ed03@outlook.com>
2021-03-13 11:55:00 -0300

    Add new JS tests

--- draft-files/module-14/test 02/script.js
+++ draft-files/module-14/test 02/script.js
@@ -9,14 +9,24 @@ let users = [
     newUser("Louis", 15)
 ]
 
+function arrayToString(){
+    return users.name + " is " + users.age + " years old";
+}
+
+console.log(users.map(arrayToString));
+
+/*Filter using '.filter'
+
 function under25(users){
     return users.age < 25;
 }
 var usersUnder25 = users.filter(under25);
+console.log(usersUnder25);*/
 
-console.log(usersUnder25);
 
-/*for(var i of users){
+/*Filter using 'for'
+
+for(var i of users){
     if(i.age < 25)
         console.log(i.name)
 }*/
