fd6029dbd541330e1c227d5395eeb1183e2a8d1b
carled7 <carlos_ed03@outlook.com>
2021-03-27 00:31:47 -0300

    Update memory game

---
+++ draft-files/memory game/scripts/script.js
@@ -0,0 +1,73 @@
+const FRONT = "card-front";
+const BACK = "card-back";
+const CARD = "card";
+const ICON = "icon";
+const FLIP = 'flip;'
+
+
+startGame();
+
+function startGame() {
+    initializeCards(game.createCardsFromTechs());
+    //console.log(game.cards);
+}
+
+function initializeCards() {
+    let gameBoard = document.getElementById('gameBoard');
+
+    game.cards.forEach(card => {
+
+        let cardElement = document.createElement('div');
+        cardElement.id = card.id;
+        cardElement.classList.add(CARD);
+        cardElement.dataset.icon = card.icon;
+
+        createCardContent(card, cardElement);
+
+        cardElement.addEventListener('click', flipCard);
+        gameBoard.appendChild(cardElement);
+
+    });
+}
+
+function createCardContent(card, cardElement) {
+    createCardFace(FRONT, card, cardElement);
+    createCardFace(BACK, card, cardElement);
+}
+
+function createCardFace(face, card, element) {
+
+    let cardElementFace = document.createElement('div');
+    cardElementFace.classList.add(face);
+    if (face == FRONT) {
+        let iconElement = document.createElement('img');
+        iconElement.classList.add(ICON);
+        iconElement.src = "./assets/" + card.icon + ".png";
+        cardElementFace.appendChild(iconElement);
+    } else {
+        cardElementFace.innerHTML = "&lt/&gt";
+    }
+    element.appendChild(cardElementFace);
+}
+
+function flipCard() {
+
+    if (game.setCard(this.id)) {
+        this.classList.add("flip");
+        //console.log(this.secondCard.icon);
+
+        if (game.checkMatch()) {
+            game.clearCards();
+        } else {
+            setTimeout(() => {
+                let firstCardView = document.getElementById(game.firstCard.id);
+                let secondCardView = document.getElementById(game.secondCard.id);
+
+                firstCardView.classList.remove(FLIP);
+                secondCardView.classList.remove(FLIP);
+                game.clearCards();
+            }, 1000);
+        }
+
+    }
+}
