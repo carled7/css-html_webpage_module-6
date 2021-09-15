5c9627d1e026ece3f8c84083d4e13e4e225ed265
carled7 <carlos_ed03@outlook.com>
2021-09-15 17:33:14 -0300

    scripting with nodeJS

--- draft-files/module-21/script.js
+++ draft-files/module-21/script.js
@@ -1,1 +1,22 @@
-console.log("Hello World!");
+//console.log("Hello World!");
+
+let args = process.argv.slice(2);
+let result;
+
+function soma(x, y){
+    return Number(x) + Number(y);
+}
+function mult(x, y){
+    return Number(x) * Number(y);
+}
+
+if(args[0] == "m"){
+    result = mult(args[1], args[2]);
+}else if(args[0] == "s"){
+    result = soma(args[1], args[2]);
+}else{
+    console.log("Invalid input");
+}
+
+
+console.log(result);
