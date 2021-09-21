494a61b2c149f1e72a930f3331b79a01febd1f35
carled7 <carlos_ed03@outlook.com>
2021-09-21 18:47:59 -0300

    update repo

--- draft-files/module-21/imageResize/app.js
+++ draft-files/module-21/imageResize/app.js
@@ -1,4 +1,6 @@
 const sharp = require('sharp');
+const compress_images = require("compress-images");
+//const fs = require('fs');
 
 let path = process.argv[2];
 let width = Number(process.argv[3]);
@@ -10,8 +12,26 @@ function resizeImg(path, width){
             console.log(err);
         }else{
             console.log("Done resizing");
+            compressImg("./outputImg.jpg", "./compressedImg.jpg");
         }
     })
+}
+
+
+function compressImg(inputPath, outputPath){
+    compress_images(inputPath, outputPath, { compress_force: false, statistic: true, autoupdate: true }, false,
+                    { jpg: { engine: "mozjpeg", command: ["-quality", "60"] } },
+                    { png: { engine: "pngquant", command: ["--quality=20-50", "-o"] } },
+                    { svg: { engine: "svgo", command: "--multipass" } },
+                    { gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] } },
+    function (error, completed, statistic) {
+        console.log("-------------");
+        console.log(error);
+        console.log(completed);
+        console.log(statistic);
+        console.log("-------------");
+    }
+    )
 }
 
 resizeImg(path, width);
