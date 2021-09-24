3f12f9e8c569959fc8c16cc7933d12a16240ef84
carled7 <carlos_ed03@outlook.com>
2021-09-24 15:59:10 -0300

    server with node

--- draft-files/module-21/Http/app.js
+++ draft-files/module-21/Http/app.js
@@ -1,9 +1,32 @@
 
 const http = require("http");
+const url = require("url");
+const fs = require("fs");
 
 http.createServer((request, response) =>{
 
-    response.writeHead(200, {'Content-Type': 'text-plain'});
-    response.end('Hello world');
+    //let path = url.parse(request.url).pathname;
+    let path = request.url;
+    let fileName = "." + path + ".html";
 
-}).listen(3000);
+    fs.readFile(fileName, (err, data) =>{
+
+        if(err){
+            response.writeHead(404,{'Content-Type': 'text-html'});
+            response.end("<h1>Not Found</h1>");
+        }else{
+            response.writeHead(200,{'Content-Type': 'text-html'});
+            response.write(data);
+            response.end();
+        }
+    }) 
+
+
+
+}).listen(3000, (err) => {
+    if(err){
+        console.log(err);
+    }else{
+        console.log("Servido rodando na porta 3000");
+    }
+});
