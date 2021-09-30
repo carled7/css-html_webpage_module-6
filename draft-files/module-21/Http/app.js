0e45a6a88ac62e5946d90d64dacedf59b425e37f
carled7 <carlos_ed03@outlook.com>
2021-09-30 15:37:15 -0300

    update repo

--- draft-files/module-21/Http/app.js
+++ draft-files/module-21/Http/app.js
@@ -3,25 +3,43 @@ const http = require("http");
 const url = require("url");
 const fs = require("fs");
 
-http.createServer((request, response) =>{
+function handleFile(req, res, callback){
 
     //let path = url.parse(request.url).pathname;
-    let path = request.url;
+    let path = req.url;
     let fileName = "." + path + ".html";
 
     fs.readFile(fileName, (err, data) =>{
-
         if(err){
-            response.writeHead(404,{'Content-Type': 'text-html'});
-            response.end("<h1>Not Found</h1>");
+            if(callback){
+                if(!callback(res, res)){
+                    res.writeHead(404,{'Content-Type': 'text-html'});
+                    res.end("<h1>Not Found</h1>");
+                }
+            }
+
         }else{
-            response.writeHead(200,{'Content-Type': 'text-html'});
-            response.write(data);
-            response.end();
+            res.writeHead(200,{'Content-Type': 'text-html'});
+            res.write(data);
+            res.end();
         }
     }) 
+}
 
+function handleRequest (req, res){
 
+    let path = req.url;
+    if(path == "/teste"){
+
+        res.end("teste");
+        return true;
+    }
+    return false;
+}
+
+http.createServer((request, response) =>{
+
+    handleFile(request, response, handleRequest);
 
 }).listen(3000, (err) => {
     if(err){
